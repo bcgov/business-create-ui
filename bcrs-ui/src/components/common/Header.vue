d466987c8de1f50da9cf210315e09a675539226f
Cameron Bowler <cameron@freshworks.io>
2020-01-06 12:15:42 -0800

    Pr Updates

--- bcrs-ui/src/components/common/Header.vue
+++ bcrs-ui/src/components/common/Header.vue
@@ -17,57 +17,57 @@ export default class Header extends Vue {}
 </script>
 
 <style lang="scss" scoped>
-  @import "../../assets/styles/theme.scss";
+@import "../../assets/styles/theme.scss";
 
-  $app-header-font-color: #ffffff;
+$app-header-font-color: #ffffff;
 
-  .app-header {
-    height: 70px;
-    color: $app-header-font-color;
-    border-bottom: 2px solid $BCgovGold5;
-    background-color: $BCgovBlue5;
+.app-header {
+  height: 70px;
+  color: $app-header-font-color;
+  border-bottom: 2px solid $BCgovGold5;
+  background-color: $BCgovBlue5;
 
-    .container {
-      display: flex;
-      align-items: center;
-      height: 100%;
-      padding-top: 0;
-      padding-bottom: 0;
-    }
-  }
-
-  .brand {
+  .container {
     display: flex;
     align-items: center;
-    padding-right: 1rem;
-    text-decoration: none;
-    color: inherit;
+    height: 100%;
+    padding-top: 0;
+    padding-bottom: 0;
   }
+}
 
+.brand {
+  display: flex;
+  align-items: center;
+  padding-right: 1rem;
+  text-decoration: none;
+  color: inherit;
+}
+
+.brand__image {
+  display: block;
+  margin-right: 1.25rem;
+  max-height: 70px;
+}
+
+.brand__title {
+  font-size: 1rem;
+  font-weight: 700;
+}
+
+@media (max-width: 600px) {
   .brand__image {
-    display: block;
-    margin-right: 1.25rem;
-    max-height: 70px;
+    margin-right: 0.75rem;
+    margin-left: -0.15rem;
   }
 
   .brand__title {
     font-size: 1rem;
-    font-weight: 700;
+    line-height: 1.25rem;
   }
 
-  @media (max-width: 600px) {
-    .brand__image {
-      margin-right: 0.75rem;
-      margin-left: -0.15rem;
-    }
-
-    .brand__title {
-      font-size: 1rem;
-      line-height: 1.25rem;
-    }
-
-    .brand__title--wrap {
-      display: block;
-    }
+  .brand__title--wrap {
+    display: block;
   }
+}
 </style>
