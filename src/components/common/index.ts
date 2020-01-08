c5f6ec638f05f0c67df93a496089d99aea875cd1
Cameron Bowler <cameron@freshworks.io>
2020-01-08 11:39:09 -0800

    Removed Unused Component

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,9 +1,7 @@
 import EntityInfo from './EntityInfo.vue'
-import Header from './Header.vue'
 import ResourceExample from './ResourceExample.vue'
 
 export {
   EntityInfo,
-  Header,
   ResourceExample
 }
