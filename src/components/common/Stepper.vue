5a21f38e8f5c8584d4da0bb51f57f4e5d0edfe0d
Severin Beauvais <severin.beauvais@gmail.com>
2020-03-04 14:15:22 -0800

    - added line between steps in prototype stepper

--- src/components/common/Stepper.vue
+++ src/components/common/Stepper.vue
@@ -1,12 +1,17 @@
 <template>
-  <v-container id="step-buttons-container" class="list-item justify-space-around py-8">
+  <v-container id="step-buttons-container">
     <template v-for="(step, index) in steps">
-      <div class="text-center align-self-start" v-bind:key="index">
-        <v-btn :id=step.id outlined fab color="primary" :to=step.to :disabled=step.disabled>
-          <v-icon>{{step.icon}}</v-icon>
-        </v-btn>
-        <v-icon size="30" color="green darken-1" v-show=step.valid>mdi-check-circle</v-icon>
-        <div class="pre-line">{{step.text}}</div>
+      <div class="step" v-bind:key="index">
+        <div class="step__indicator">
+          <div class="step__line"></div>
+          <v-btn class="step__icon" :id=step.id outlined fab color="primary" :to=step.to :disabled=step.disabled>
+            <v-icon>{{step.icon}}</v-icon>
+          </v-btn>
+          <v-icon class="step__icon2" size="30" color="green darken-1" v-show=step.valid>
+            mdi-check-circle
+          </v-icon>
+        </div>
+        <div class="step__label pre-line">{{step.text}}</div>
       </div>
     </template>
   </v-container>
@@ -177,14 +182,58 @@ export default class Stepper extends Vue {
 @import '@/assets/styles/theme.scss';
 
 #step-buttons-container {
+  display: flex;
+  justify-content: space-evenly;
+  margin: 0;
+  padding: 2rem 0;
   background: $BCgovInputBG;
 }
 
-.mdi-check-circle {
+.step {
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 auto;
+  align-items: center;
+  justify-content: center;
+}
+
+.step__indicator {
+  position: relative;
+  width: 100%;
+  text-align: center;
+}
+
+.step__line {
+  position: absolute;
+  top: 50%;
+  left: 0;
+  height: 2px;
+  width: 100%;
+  background-color: $gray5;
+  transform: translateX(-50%);
+}
+
+.step:first-child .step__line {
+  display: none;
+}
+
+.step__icon {
+  position: relative;
+  background-color: $BCgovInputBG;
+  z-index: 2;
+}
+
+.step__icon2 {
   position: relative;
   margin-top: -32px;
   margin-left: -16px;
   background: $BCgovInputBG;
   border-radius: 50%;
+  z-index: 3;
+}
+
+.step__label {
+  margin-top: 10px;
+  text-align: center;
 }
 </style>
