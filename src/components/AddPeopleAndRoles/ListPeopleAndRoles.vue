2b4f5b4c92a860a1fa3857bedef3fa8e86a477c1
Cameron Bowler <cameron@freshworks.io>
2020-03-10 10:19:27 -0700

    Summary Complete

--- src/components/AddPeopleAndRoles/ListPeopleAndRoles.vue
+++ src/components/AddPeopleAndRoles/ListPeopleAndRoles.vue
@@ -1,5 +1,22 @@
 <template>
   <div id="people-roles-list">
+    <v-card flat>
+      <!-- Summary Header -->
+      <div class="people-roles-summary-header" v-if="isSummary">
+        <v-icon>mdi-account</v-icon>
+        <label class="people-roles-title"><strong>People and Roles</strong></label>
+      </div>
+
+      <!-- Summary Warning -->
+      <div v-if="isSummary && true" class="defineCompanyStepErrorMessage">
+      <span>
+        <v-icon color="#1976d2">mdi-information-outline</v-icon>
+        This step is not complete.
+        <router-link :to="{ path: '/add-people-roles', query: { showErrors: true } }">
+          Return to this step to complete it.
+        </router-link>
+      </span>
+      </div>
 
       <!-- List Headers -->
       <v-row class="people-roles-header list-item__subtitle" no-gutters>
@@ -7,17 +24,18 @@
           <span>{{title}}</span>
         </v-col>
         <!-- Spacer Column -->
-        <v-col sm="1"></v-col>
+        <v-col sm="1" v-if="!isSummary"></v-col>
       </v-row>
 
       <!-- List Content -->
       <v-row
-        class="people-roles-content list-item__subtitle"
+        class="people-roles-content"
+        :class="{ 'list-item__subtitle': !isSummary }"
         v-for="(person, index) in personList"
         :key="index"
         no-gutters>
         <v-col class="text-truncate">
-          <v-tooltip bottom :disabled="formatName(person).length < 25" color="primary">
+          <v-tooltip top :disabled="formatName(person).length < 25" color="primary">
             <template v-slot:activator="{ on }">
               <span v-on="on" class="people-roles-title"><strong>{{formatName(person)}}</strong></span>
             </template>
@@ -28,15 +46,17 @@
           <base-address :address="person.address.mailingAddress" />
         </v-col>
         <v-col>
-          <base-address :address="person.address.deliveryAddress" />
+          <p v-if="isSame(person.address.mailingAddress, person.address.deliveryAddress)">Same as Mailing Address</p>
+          <base-address v-else :address="person.address.deliveryAddress"/>
         </v-col>
         <v-col>
           <v-col v-for="(role, index) in person.roles" :key="index" class="col-roles">
             <span>{{role}}</span>
           </v-col>
         </v-col>
-        <v-col sm="1">
-          <!-- Edit menu -->
+
+        <!-- Actions Column -->
+        <v-col sm="1" v-if="!isSummary">
           <div class="actions">
             <span class="edit-action">
               <v-btn small text color="primary" :disabled="false"
@@ -66,40 +86,46 @@
           </div>
         </v-col>
       </v-row>
-
+    </v-card>
   </div>
 </template>
 
 <script lang="ts">
 // Libraries
-import { Component, Vue, Prop } from 'vue-property-decorator'
-import { State } from 'vuex-class'
+import { Component, Prop, Mixins } from 'vue-property-decorator'
 
 // Components
 import BaseAddress from 'sbc-common-components/src/components/BaseAddress.vue'
 
+// Mixins
+import CommonMixin from '@/mixins/common-mixin'
+
 // Interfaces
 import { OrgPersonIF } from '@/interfaces'
-
-// Enums
-import { EntityTypes } from '@/enums'
 
 @Component({
   components: {
     BaseAddress
   }
 })
-export default class ListPeopleAndRoles extends Vue {
+export default class ListPeopleAndRoles extends Mixins(CommonMixin) {
+  // Store Properties
   @Prop({ default: '' })
   private personList: Array<OrgPersonIF>
 
-  private isTrue = true
+  @Prop({ default: false })
+  private isSummary: boolean
 
+  // Local Properties
   private tableHeaders: Array<string> = ['Name', 'Mailing Address', 'Delivery Address', 'Roles']
 
-  private formatName (person: any): string {
-    return person.orgName ? person.orgName
-      : `${person.firstName} ${person.middleName || ''} ${person.lastName}`
+  /**
+   * Return the appropriate Corporation or filers name
+   * @param filing The filing body
+   */
+  private formatName (filing: any): string {
+    return filing.orgName ? filing.orgName
+      : `${filing.firstName} ${filing.middleName || ''} ${filing.lastName}`
   }
 
   private doSomething (id: number): void {
@@ -110,16 +136,31 @@ export default class ListPeopleAndRoles extends Vue {
 
 <style lang="scss" scoped>
 #people-roles-list {
-  padding: 0!important;
+  margin-top: 1rem;
+}
+
+.people-roles-summary-header {
+  display: flex;
+  background-color: rgba(1, 51, 102, 0.15);
+  padding: 1.25rem;
+}
+
+.defineCompanyStepErrorMessage {
+  padding-top: 1.25rem;
+  padding-left: 1.25rem;
+  font-weight: bold;
+  color: #1976d2;
 }
 
 .people-roles-header {
+  padding: .5rem 1.25rem .5rem 1.25rem;
   font-size: 12px;
+  margin-top: 1rem;
 }
 
 .people-roles-content {
   margin-top: .5rem;
-  padding-top: .5rem;
+  padding: .5rem 1.25rem .5rem 1.25rem;
   border-top: 1px solid #d1d1d1;
   font-size: 14px;
 
