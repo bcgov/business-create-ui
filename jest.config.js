7741e8f494a38e5ce14faeeb231609c88e55b0e6
Odysseus Chiu <odysseus@highwaythreesolutions.com>
2020-03-23 07:45:22 -0700

    #3071 - update sbc-common-components, change event to listen to for sign in, fix tests

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,11 @@ module.exports = {
   globals: {
     'ts-jest': {
       diagnostics: false,
-      isolatedModules: true
+      isolatedModules: true,
+      babelConfig: true
     }
   },
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   transformIgnorePatterns: []
+
 }
--- src/views/auth/Signin.vue
+++ src/views/auth/Signin.vue
@@ -1,6 +1,6 @@
 <template>
 <sbc-signin
-    @sync-user-profile-ready="redirectOnLogin()"
+    @keycloak-session-ready="redirectOnLogin()"
   ></sbc-signin>
 </template>
 
@@ -23,6 +23,7 @@ import { ActionBindingIF } from '@/interfaces'
 export default class SigninView extends Vue {
   @Action setAuthenticated: ActionBindingIF
 
+  // Called when keycloak-session-ready event is emitted which indicates the user is authenticated
   private async redirectOnLogin () {
     this.setAuthenticated(true)
     this.$router.push(
@@ -62,6 +63,7 @@ export default class SigninView extends Vue {
         }
       }
     }
+    console.log('queryParam', paramConfig)
     return paramConfig
   }
 }
