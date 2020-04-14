50274e2ff9a242b93bf4f43127ac3a438650b179
cameron-freshworks <53542131+cameron-freshworks@users.noreply.github.com>
2020-04-14 09:05:54 -0700

    Step3 help tips (#89)
    * Implemented Share Help Section
    
    * Pr Update

--- src/views/CreateShareStructure.vue
+++ src/views/CreateShareStructure.vue
@@ -7,7 +7,40 @@
       Add at least one class of shares. A share class consists of the name of the class, the maximum number of shares in
       the class (including any associated Series), a par value for the class, and the currency the shares are valued in.
     </p>
-    Your application must include the following:
+
+    <!-- Help Section -->
+    <span class="help-btn" @click="helpToggle = !helpToggle">
+      <v-icon color="#1976d2" style="padding-right: 5px">mdi-help-circle-outline</v-icon>
+      <span v-if="!helpToggle">Help with Share Structure</span>
+      <span v-else>Hide Help</span>
+    </span>
+    <section v-show="helpToggle" class="share-structure-help">
+      <header id="share-structure-help-header"><h2>Share Structure Help</h2></header>
+      <p>
+        An incorporated business must issue shares. These shares represent ownership interest in the company and give
+        the shareholder a say in how the company is being run. For most small companies starting out, a simple share
+        structure with just one class of shares (and no series) is typical.
+      </p>
+      <p>
+        The sample below is for a typical share structure with just one class of shares. If there is more than one class
+        of shares, each class must be assigned an identifying name such as Class A, Class B, etc.
+      </p>
+      <div id="share-structure-sample">
+        <ListShareClass :shareClasses="sharesHelpSample"/>
+      </div>
+      <p><small>Sample Share Structure</small></p>
+      <h4>Important Information About the Company's Share Structure</h4>
+      <p>
+        The staff at the Corporate Registry cannot provide advice on how to set up your company's share structure. If
+        you do not understand what an authorized share structure is or what its purpose is or believe you need a more
+        complex share structure, you should seek professional advice or purchase an incorporation guide for detailed
+        information and infrastructure on establishing an authorized share structure.
+      </p>
+      <p>Refer to <a href="">helpLinkGoesHere</a> to obtain more information on incorporating a company</p>
+      <u class="help-btn" @click="helpToggle = !helpToggle"><small>Hide Help</small></u>
+    </section>
+
+    <p>Your application must include the following:</p>
     <ul>
       <li>
         <v-icon color="blue" v-if="shareClasses.length > 0" class="cp-valid">mdi-check</v-icon>
@@ -101,6 +134,20 @@ export default class CreateShareStructure extends Vue {
     hasRightsOrRestrictions: false
   }
 
+  private sharesHelpSample: ShareClassIF[] = [{
+    'id': 1,
+    'priority': 0,
+    'type': 'Class',
+    'name': 'Sample Shares',
+    'hasMaximumShares': true,
+    'maxNumberOfShares': 1000,
+    'hasParValue': true,
+    'parValue': 1,
+    'currency': 'CAD',
+    'hasRightsOrRestrictions': true,
+    'series': []
+  }]
+
   private showShareStructureForm: boolean = false
   private addEditInProgress: boolean = false
   private currentShareStructure: ShareClassIF | null = null
@@ -108,6 +155,7 @@ export default class CreateShareStructure extends Vue {
   private activeIndex: number = -1
   private parentIndex: number = -1
   private nextId: number = -1
+  private helpToggle: boolean = false
 
   // Lifecycle methods
   mounted (): void {
@@ -230,29 +278,68 @@ export default class CreateShareStructure extends Vue {
 </script>
 
 <style lang="scss" scoped>
+@import '@/assets/styles/theme.scss';
+
 [class^="col"] {
   padding-top: 0;
   padding-bottom: 0;
 }
+
 .add-share-structure-container {
   margin-top: 1rem;
   margin-bottom:1rem;
 }
+
 ul {
   padding-top: 0.5rem;
   list-style: none;
   margin-left: 0;
   padding-left: 1rem
 }
+
 li {
   padding-top:0.25rem
 }
+
 p{
   padding-top: 0.5rem;
 }
+
 .btn-panel {
   padding: 2rem 0 2rem 0;
 }
+
+.help-btn {
+  cursor: pointer;
+  color: #1976d2;
+  vertical-align:middle;
+}
+
+.share-structure-help {
+  margin: 2rem 0;
+  border-top: 1px dashed $gray6;
+  border-bottom: 1px dashed $gray6;
+  padding: 1rem 0;
+
+  #share-structure-help-header {
+    display: flex;
+    justify-content: center;
+  }
+
+  #share-structure-sample {
+    padding: 1rem 0;
+  }
+
+  h2, h4 {
+    padding: 1rem 0;
+  }
+
+  u {
+    display: flex;
+    direction: rtl;
+  }
+}
+
 .chk-list-item-txt {
   margin-left: 0.5rem;
 }
