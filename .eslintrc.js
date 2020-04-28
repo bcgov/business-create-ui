2d8ce77e8aaa34bb76a04ef864ba1e6ce5b0e5a8
Odysseus Chiu <odysseus@highwaythreesolutions.com>
2020-04-28 10:09:43 -0700

    2556 - update email validation (#104)

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'max-len': ['warn', { code: 120 }]
+    'max-len': ['warn', { code: 120, 
+                          ignoreRegExpLiterals: true }]
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
--- src/components/DefineCompany/BusinessContactInfo.vue
+++ src/components/DefineCompany/BusinessContactInfo.vue
@@ -103,7 +103,10 @@ export default class BusinessContactInfo extends Vue {
   // Rules
   private emailRules = [
     (v: string) => !!v || 'Email address is required',
-    (v: string) => /.+@.+\..+/.test(v) || 'Valid email is required'
+    (v: string) => {
+      const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return pattern.test(v) || 'Valid email is required'
+    }
   ]
 
   private phoneRules = [
