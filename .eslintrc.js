715e5f92bdf3aca8a3713f5e487eabf183de9597
Lekshmi Mallika <lekshmi.mallika@aot-technologies.com>
2020-04-30 13:53:10 -0700

    UI fixes (#106)
    * Changes to pass the effective date string only for future effective filings
    Change to pass approved name as legal name in the NR section of the filing request for the filer to use
    Updated the tests
    Lint fix
    
    * Fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'max-len': ['warn', { code: 120, 
-                          ignoreRegExpLiterals: true }]
+    'max-len': ['warn', { code: 120,
+      ignoreRegExpLiterals: true }]
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
