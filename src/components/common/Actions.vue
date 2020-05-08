be06004e2c1d5fdee5781bfbc6d6f0143e3d0b87
Cameron Bowler <cameron@freshworks.io>
2020-05-08 09:19:02 -0700

    Bug fix

--- src/components/common/Actions.vue
+++ src/components/common/Actions.vue
@@ -164,7 +164,7 @@ export default class Actions extends Mixins(DateMixin, FilingTemplateMixin, Lega
     this.setIsFilingPaying(true)
     let filingComplete
 
-    if (!this.isValidDateTime(this.effectiveDate)) {
+    if (this.effectiveDate && !this.isValidDateTime(this.effectiveDate)) {
       this.setIsIncorporationDateTimeValid(false)
       this.setIsFilingPaying(false)
       window.scrollTo({ top: 1250, behavior: 'smooth' })
