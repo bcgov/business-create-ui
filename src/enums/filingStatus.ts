2c7b0235e93cd5970229464ad656a60e2124150f
Odysseus Chiu <odysseus@highwaythreesolutions.com>
2020-06-17 13:26:40 -0700

    4027 - filed IA handling

---
+++ src/enums/filingStatus.ts
@@ -0,0 +1,10 @@
+export enum FilingStatus {
+  COMPLETED = 'COMPLETED',
+  DRAFT = 'DRAFT',
+  EPOCH = 'EPOCH',
+  ERROR = 'ERROR',
+  NEW = 'NEW',
+  PAID = 'PAID',
+  PENDING = 'PENDING',
+  PENDING_CORRECTION = 'PENDING_CORRECTION',
+}
---
+++ tests/unit/AccountAuthorizationDialog.spec.ts
@@ -0,0 +1,76 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import { getVuexStore } from '@/store'
+import { shallowMount, mount } from '@vue/test-utils'
+import { AccountAuthorizationDialog } from '@/components/dialogs'
+
+Vue.use(Vuetify)
+
+const vuetify = new Vuetify({})
+const store = getVuexStore()
+
+// Prevent the warning "[Vuetify] Unable to locate target [data-app]"
+document.body.setAttribute('data-app', 'true')
+
+describe('Account Authorization Dialog', () => {
+  it('renders the component properly', () => {
+    const wrapper = shallowMount(AccountAuthorizationDialog,
+      {
+        vuetify,
+        store,
+        propsData: { dialog: true }
+      })
+    expect(wrapper.attributes('contentclass')).toBe('account-authorization-dialog')
+    expect(wrapper.isVisible()).toBe(true)
+    expect(wrapper.find('#dialog-title').text()).toBe('Account Authorization')
+    expect(wrapper.findAll('p').length).toBe(1)
+    expect(wrapper.findAll('p').at(0).text())
+      .toContain('This account appears to be unable to access this name request, incorporation application')
+    expect(wrapper.find('#dialog-exit-button').exists()).toBe(true)
+    expect(wrapper.find('#dialog-retry-button').exists()).toBe(true)
+
+    wrapper.destroy()
+  })
+
+  it('emits an event when Exit button is clicked', async () => {
+    const wrapper = mount(AccountAuthorizationDialog,
+      {
+        vuetify,
+        store,
+        propsData: { dialog: true }
+      })
+
+    expect(wrapper.emitted('exit')).toBeUndefined()
+
+    // verify and click Exit button
+    const exitButton = wrapper.find('#dialog-exit-button')
+    expect(exitButton.text()).toBe('Exit')
+    exitButton.trigger('click')
+    await Vue.nextTick()
+
+    expect(wrapper.emitted('exit').length).toBe(1)
+
+    wrapper.destroy()
+  })
+
+  it('emits an event when retry button is clicked', async () => {
+    const wrapper = mount(AccountAuthorizationDialog,
+      {
+        vuetify,
+        store,
+        propsData: { dialog: true }
+      })
+
+    expect(wrapper.emitted('retry')).toBeUndefined()
+
+    // verify and click retry button
+    const retryButton = wrapper.find('#dialog-retry-button')
+    expect(retryButton.text()).toBe('Retry')
+    retryButton.trigger('click')
+    await Vue.nextTick()
+
+    expect(wrapper.emitted('retry').length).toBe(1)
+
+    wrapper.destroy()
+  })
+})
