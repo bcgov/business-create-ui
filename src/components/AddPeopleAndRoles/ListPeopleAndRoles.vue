e392c7ea251cf3f05c36fc63d0b4473348568b9f
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2020-09-21 10:01:45 -0700

    Update ListPeopleAndRoles.vue (#164)
    - fixed incorrect icon on review page

--- src/components/AddPeopleAndRoles/ListPeopleAndRoles.vue
+++ src/components/AddPeopleAndRoles/ListPeopleAndRoles.vue
@@ -12,7 +12,7 @@
     <div id="people-roles-summary" v-if="isSummary">
       <!-- Summary Header -->
       <div class="people-roles-summary-header" >
-        <v-icon>mdi-account</v-icon>
+        <v-icon>mdi-account-multiple-plus</v-icon>
         <label class="people-roles-title"><strong>People and Roles</strong></label>
       </div>
 
