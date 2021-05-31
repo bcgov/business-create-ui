528cbd81543a2eb001f4eed55f14efa04749900a
Cameron-freshworks <cameron@freshworks.io>
2021-05-31 11:33:06 -0700

    Updated typing

--- src/components/IncorporationAgreement/AgreementType.vue
+++ src/components/IncorporationAgreement/AgreementType.vue
@@ -64,7 +64,7 @@ export default class AgreementType extends Vue {
   private isSummary: boolean
 
   // Global getters
-  @Getter getIncorporationAgreement!: IncorporationAgreementTypeIF
+  @Getter getIncorporationAgreement!: Array<IncorporationAgreementTypeIF>
 
   // Actions
   @Action setIncorporationAgreementStepData!: ActionBindingIF
