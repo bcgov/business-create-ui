e3242b8476548774869849a6443852c198e4549d
eve-git <54647458+eve-git@users.noreply.github.com>
2021-09-21 09:54:29 -0700

    File and Pay button is enabled even though the required fields are no… (#243)
    * File and Pay button is enabled even though the required fields are not filled.#8917
    
    * update coding style
    
    1. update the coding style in the changes in state-getters, in order to read easily
    2. revert back the changes in stepper because I realized that the original code may for design purpose and the change is not in the scope of the ticket 8917

--- src/store/getters/state-getters.ts
+++ src/store/getters/state-getters.ts
@@ -295,13 +295,16 @@ export const isApplicationValid = (state: StateIF): boolean => {
     getCreateMemorandumStep(state).validationDetail.valid
   )
 
+  // Validate different steps for Base Companies vs Coops
+  const isDocumentValid = isBaseCompany(state) ? isBaseStepsValid : isCoopStepsValid
+
+  const isCertifyValid = getCertifyState(state).valid && !!getCertifyState(state).certifiedBy
+
   return (
     getDefineCompanyStep(state).valid &&
     getAddPeopleAndRoleStep(state).valid &&
-    // Validate different steps for Base Companies vs Coops
-    isBaseCompany(state) ? isBaseStepsValid : isCoopStepsValid &&
-    getCertifyState(state).valid &&
-    !!getCertifyState(state).certifiedBy
+    isDocumentValid &&
+    isCertifyValid
   )
 }
 
