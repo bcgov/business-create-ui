9c0de29a46cdbc4bc5a525d1e8b99996cd7735cf
eve-git <54647458+eve-git@users.noreply.github.com>
2021-10-27 10:12:13 -0700

    #9767 create a filing-template-mixin test case (#267)
    * create filing-template-mixin test case for coop nameTranslations
    
    * update the test data to be more generic

---
+++ tests/unit/mixin-tester.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-none" />
+</template>
+
+<script lang="ts">
+// Libraries
+import { Component, Mixins } from 'vue-property-decorator'
+
+// Mixins
+import { FilingTemplateMixin } from '@/mixins'
+
+@Component({})
+export default class MixinTester extends Mixins(
+  FilingTemplateMixin
+) {}
+</script>
+
+<style>
+</style>
