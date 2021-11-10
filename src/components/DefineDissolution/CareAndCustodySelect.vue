6078eb27b5c5317215fe56ecd66fb68bc289e88d
Cameron-freshworks <cameron@freshworks.io>
2021-11-10 10:24:12 -0800

    Updated testing for CareAndCustodySelect

--- src/components/DefineDissolution/CareAndCustodySelect.vue
+++ src/components/DefineDissolution/CareAndCustodySelect.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card flat class="rounded-4 pt-11">
+  <v-card flat id="care-and-custody-select" class="rounded-4 pt-11">
     <div class="section-container py-6 px-8 " :class="{ 'invalid-section': showErrorSummary }">
       <v-row no-gutters>
         <v-col cols="12" md="3" lg="3">
@@ -11,7 +11,7 @@
             v-model="liquidatorOrCustodian"
             @change="changeCareAndCustodyType"
           >
-            <v-radio :value=RoleTypes.LIQUIDATOR>
+            <v-radio id="liquidator-radio-btn" :value=RoleTypes.LIQUIDATOR>
               <template slot="label">
                 <span class="care-and-custody-option">
                   A <strong>Liquidator</strong> has been appointed for the care and custody of the
@@ -19,7 +19,7 @@
                 </span>
               </template>
             </v-radio>
-            <v-radio :value=RoleTypes.CUSTODIAN>
+            <v-radio id="custodian-radio-btn" :value=RoleTypes.CUSTODIAN>
               <template slot="label">
                 <span class="care-and-custody-option">
                   A <strong>Custodian of Records</strong> has been appointed for the care and custody of the Cooperative
@@ -37,19 +37,12 @@
 <script lang="ts">
 // Libraries
 import { Component, Vue, Prop } from 'vue-property-decorator'
-import { Action, Getter } from 'vuex-class'
-
-// Interfaces & enums
-import { ActionBindingIF, DissolutionStatementIF, KeyValueIF, RolesIF } from '@/interfaces'
-import { DissolutionStatementTypes, RoleTypes } from '@/enums'
+import { RoleTypes } from '@/enums'
 
 @Component
 export default class CareAndCustodySelect extends Vue {
   @Prop({ default: false })
   private readonly showErrorSummary: boolean
-
-  // Global setters
-  @Action setDissolutionStatementStepData!: ActionBindingIF
 
   private readonly RoleTypes = RoleTypes
   private liquidatorOrCustodian: RoleTypes = null
---
+++ tests/unit/CareAndCustodySelect.spec.ts
@@ -0,0 +1,66 @@
+import Vue from 'vue'
+import { wrapperFactory } from '../jest-wrapper-factory'
+import { CareAndCustodySelect } from '@/components/DefineDissolution'
+import { DissolutionResources } from '@/resources'
+import { RoleTypes } from '@/enums'
+
+// Test Case Data
+const custodianTestCases = [
+  {
+    entityType: 'CP'
+  },
+  {
+    entityType: 'BEN'
+  },
+  {
+    entityType: 'BC'
+  }
+]
+
+for (const mock of custodianTestCases) {
+  describe(`Care and Custody Select component for a ${mock.entityType}`, () => {
+    let wrapper: any
+
+    beforeEach(() => {
+      wrapper = wrapperFactory(
+        CareAndCustodySelect,
+        null,
+        { entityType: mock.entityType },
+        null,
+        DissolutionResources
+      )
+    })
+
+    it('renders the component properly', () => {
+      expect(wrapper.find('#care-and-custody-select').exists()).toBe(true)
+    })
+
+    it('displays the radio buttons', () => {
+      expect(wrapper.find('.case-and-custody-option-list').exists()).toBe(true)
+    })
+
+    it('changes the role type selection to Liquidator when the radio button is selected', async () => {
+      // Verify default model state
+      expect(wrapper.vm.liquidatorOrCustodian).toBeNull()
+
+      const custodianRadioBtn = wrapper.find('#liquidator-radio-btn')
+      custodianRadioBtn.trigger('click')
+      await Vue.nextTick()
+
+      // Verify new model state
+      expect(wrapper.vm.liquidatorOrCustodian).toBe(RoleTypes.LIQUIDATOR)
+    })
+
+    it('changes the role type selection to Custodian when the radio button is selected', async () => {
+      // Verify default model state
+      expect(wrapper.vm.liquidatorOrCustodian).toBeNull()
+
+      const custodianRadioBtn = wrapper.find('#custodian-radio-btn')
+      custodianRadioBtn.trigger('click')
+      await Vue.nextTick()
+
+      // Verify new model state
+      expect(wrapper.vm.liquidatorOrCustodian).toBe(RoleTypes.CUSTODIAN)
+    })
+  })
+}
