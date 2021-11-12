5f24ef506bf30329a366b6cd8562aa38a4b9600a
Severin Beauvais <severin.beauvais@gmail.com>
2021-11-12 10:41:19 -0800

    - added TODO comment

--- tests/unit/TransactionalFolioNumber.spec.ts
+++ tests/unit/TransactionalFolioNumber.spec.ts
@@ -9,11 +9,12 @@ Vue.use(Vuetify)
 const vuetify = new Vuetify({})
 const store = getVuexStore()
 
+// *** TODO: finish implementing tests
+// *** TODO: use new unit test format if possible (see jest-wrapper-factory)
 describe('Transactional Folio Number component', () => {
   it('Displays expected content', () => {
     const wrapper = mount(TransactionalFolionNumber, { vuetify, store })
 
-    // *** TODO: finish implementing tests
     expect(wrapper.find('#transactional-folio-number-container').exists()).toBe(true)
 
     wrapper.destroy()
