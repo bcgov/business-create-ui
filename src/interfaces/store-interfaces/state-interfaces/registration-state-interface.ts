4b1f3ba06f257f46060bb6ebe47945ae88615e03
Severin Beauvais <severin.beauvais@gmail.com>
2022-04-13 17:43:08 -0700

    - removed conditional address validation per 11854 - deleted obsolete "registration NR" - used "tombstone NR" instead - updated unit test - app version = 3.5.15

--- src/interfaces/store-interfaces/state-interfaces/registration-state-interface.ts
+++ src/interfaces/store-interfaces/state-interfaces/registration-state-interface.ts
@@ -7,19 +7,12 @@ export interface RegistrationNameRequestIF {
   nrNumber: string
 }
 
-export const EmptyRegistrationNameRequest: RegistrationNameRequestIF = {
-  legalName: null,
-  legalType: null,
-  nrNumber: null
-}
-
 export interface RegistrationStateIF {
   defineBusinessValid: boolean
   startDate: string
   businessAddress: BusinessAddressIF
   feeAcknowledgement: boolean
   naics: NaicsIF
-  nameRequest: RegistrationNameRequestIF
   businessType: BusinessTypes
   businessNumber?: string // GPs only
 }
