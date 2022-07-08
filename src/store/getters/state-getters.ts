4a990e7bbb7679baec22e7e71a5124738e09a693
Lewis Chen <lewischenstudio@gmail.com>
2022-07-08 13:31:55 -0700

    12307: fix lint 2

--- src/store/getters/state-getters.ts
+++ src/store/getters/state-getters.ts
@@ -468,8 +468,8 @@ export const isDissolutionValid = (state: StateIF): boolean => {
   const isCourtOrderValid = isRoleStaff(state) ? getCourtOrderStep(state).valid : true
   const isStaffPaymentValid = isRoleStaff(state) ? getStaffPaymentStep(state).valid : true
 
-  // eslint-disable-next-line
-  const isEffectiveDateTimeValid = (isBaseCompany(state) || isTypeFirm(state)) ? getEffectiveDateTime(state).valid : true
+  const isEffectiveDateTimeValid = (isBaseCompany(state) || isTypeFirm(state))
+    ? getEffectiveDateTime(state).valid : true
 
   if (isTypeFirm(state)) {
     return (
