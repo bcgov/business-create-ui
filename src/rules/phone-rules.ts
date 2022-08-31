48ae03e575e152d6dda784d384d377514e17e61a
Lewis Chen <lewischenstudio@gmail.com>
2022-08-31 13:32:56 -0700

    13371: optional phone

--- src/rules/phone-rules.ts
+++ src/rules/phone-rules.ts
@@ -1,3 +1,3 @@
 export const PhoneRules: Array<Function> = [
-  (v: any) => !v || (v.length === 0 || v.length === 14) || 'Phone number is invalid'
+  (v: any) => (v.length === 0 || v.length === 14) || 'Phone number is invalid'
 ]
