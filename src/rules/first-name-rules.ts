d0bef4cade1101d602a6fe30199d7562fe3da731
Severin Beauvais <severin.beauvais@gmail.com>
2022-11-01 15:57:30 -0700

    - revert first name max length to 30 - revert middle name length to 30 - updated unit tests - app version = 4.3.5

--- src/rules/first-name-rules.ts
+++ src/rules/first-name-rules.ts
@@ -2,5 +2,5 @@ import { VuetifyRuleFunction } from '@/types'
 
 export const FirstNameRules: Array<VuetifyRuleFunction> = [
   v => !!v?.trim() || 'First name is required',
-  v => (v?.length <= 20) || 'Cannot exceed 20 characters' // maximum character count
+  v => (v?.length <= 30) || 'Cannot exceed 30 characters' // maximum character count
 ]
--- src/rules/middle-name-rules.ts
+++ src/rules/middle-name-rules.ts
@@ -3,5 +3,5 @@ import { VuetifyRuleFunction } from '@/types'
 export const MiddleNameRules: Array<VuetifyRuleFunction> = [
   v => !/^\s/g.test(v) || 'Invalid spaces', // leading spaces
   v => !/\s$/g.test(v) || 'Invalid spaces', // trailing spaces
-  v => (!v || v.length <= 20) || 'Cannot exceed 20 characters' // maximum character count
+  v => (!v || v.length <= 30) || 'Cannot exceed 30 characters' // maximum character count
 ]
