e116d2de1fc8ca7e808e4f303f3ceb23c667307a
Patrick Wei <44277752+pwei1018@users.noreply.github.com>
2023-04-21 08:31:52 -0700

    Clean up for the new domain name changes. (#535)
    * Fixed the content path issue.
    
    * Add quote to VUE_APP_PATH
    
    * Fixed the publicPath.
    
    * Clean up for the new domain name changes.
    
    * Remove openssl-legacy-provider option

--- vue.config.js
+++ vue.config.js
@@ -31,23 +31,5 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: `${process.env.VUE_APP_PATH}`,
-  devServer: {
-    proxy: {
-      // this is needed to prevent a CORS error when running locally
-      '/local-keycloak-config-url/*': {
-        target: 'https://business-create-dev.apps.silver.devops.gov.bc.ca/businesses/create/config/kc/',
-        pathRewrite: {
-          '/local-keycloak-config-url': ''
-        }
-      },
-      // this is needed to avoid a PAYBC Not Found error when running locally
-      '/status/PAYBC': {
-        target: 'https://status-api-dev.apps.silver.devops.gov.bc.ca/api/v1/status/PAYBC',
-        pathRewrite: {
-          '/status/PAYBC': ''
-        }
-      }
-    }
-  }
+  publicPath: `${process.env.VUE_APP_PATH}`
 }
