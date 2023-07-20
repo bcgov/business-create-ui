2a7cea0563c165c49dafd259564d43d60f2db7b7
Chris Gabel <Chris.Gabel@gov.bc.ca>
2023-07-20 11:58:28 -0700

    Deleting obsolete file

--- src/interfaces/store-interfaces/action-interface.ts
+++
@@ -1,7 +0,0 @@
-// I don't think this file is used any more - CRG Jul23/23
-import { ActionContext } from 'vuex'
-
-// Interface to define a Vuex Action
-export interface ActionIF {
-  (x: ActionContext<string, any>, y: any): void
-}
