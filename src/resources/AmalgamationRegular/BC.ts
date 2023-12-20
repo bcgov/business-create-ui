244fbcadebac5fd460f4c92c7bb6d668788c4255
ketaki-deodhar <ketaki.deodhar@gov.bc.ca>
2023-12-20 10:07:48 -0800

    18847-update certify text for amalgamations

--- src/resources/AmalgamationRegular/BC.ts
+++ src/resources/AmalgamationRegular/BC.ts
@@ -69,14 +69,7 @@ export const AmalgamationRegResourceBc: AmalgamationRegResourceIF = {
   },
   reviewAndConfirm: {
     completingPartyStatement: {
-      certifyStatements: [
-        'An original signature has been placed on each of those signature lines,',
-
-        `I have no reason to believe that the signature placed on a signature line is not the
-          signature of the person whose name is set out under that signature line, and`,
-
-        'I have relevant knowledge of the company and that I am authorized to make this filing.'
-      ],
+      certifyStatements: [],
       certifyClause: `Note: It is an offence to make a false or misleading statement in respect
         of a material fact in a record submitted to the Corporate Registry for filing.
         See section 427 of the Business Corporations Act.`,
