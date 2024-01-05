e749afb3b6320424df9f587eb07d8c34320145bb
tshyun24 <shaoyun.tong@gov.bc.ca>
2024-01-05 10:34:38 -0800

    lint and version

--- tests/unit/AmalgamationStatement.spec.ts
+++ tests/unit/AmalgamationStatement.spec.ts
@@ -7,24 +7,24 @@ const vuetify = new Vuetify({})
 setActivePinia(createPinia())
 
 describe('AmalgamationStatement', () => {
-    it('displays expected content', async () => {
-        const wrapper = mount(AmalgamationStatement, { vuetify })
+  it('displays expected content', async () => {
+    const wrapper = mount(AmalgamationStatement, { vuetify })
 
-        // sanity check
-        expect(wrapper.find('#amalgamation-statement')).toBeDefined()
+    // sanity check
+    expect(wrapper.find('#amalgamation-statement')).toBeDefined()
 
-        // verify radio button component
-        let radioBtns = wrapper.findAll('.radio-button')
-        expect(radioBtns.length).toBe(2)
-        expect(radioBtns.at(0).text()).toBe('With Court Approval')
-        expect(radioBtns.at(1).text()).toBe('Without Court Approval')
+    // verify radio button component
+    const radioBtns = wrapper.findAll('.radio-button')
+    expect(radioBtns.length).toBe(2)
+    expect(radioBtns.at(0).text()).toBe('With Court Approval')
+    expect(radioBtns.at(1).text()).toBe('Without Court Approval')
 
-        // verify statement text
-        let btnsText = wrapper.findAll('.statement-text')
-        expect(btnsText.length).toBe(2)
-        expect(btnsText.at(0).text()).toContain('This amalgamation has been approved by the court')
-        expect(btnsText.at(1).text()).toContain('This amalgamation has been effected without court approval.')
-        
-        wrapper.destroy()
-    })
+    // verify statement text
+    const btnsText = wrapper.findAll('.statement-text')
+    expect(btnsText.length).toBe(2)
+    expect(btnsText.at(0).text()).toContain('This amalgamation has been approved by the court')
+    expect(btnsText.at(1).text()).toContain('This amalgamation has been effected without court approval.')
+
+    wrapper.destroy()
+  })
 })
