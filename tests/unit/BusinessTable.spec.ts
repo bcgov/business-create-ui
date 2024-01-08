4b5d4d7f726830fba8616ba2619ff61a0731eb94
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2024-01-08 14:50:14 -0800

    - app version = 5.6.28 (#615)
    - misc fixes
    - added Business Status unit tests
    - added Business Table unit tests
    - added Business Table Summary unit tests
    
    Co-authored-by: Severin Beauvais <severin.beauvais@gov.bc.ca>

---
+++ tests/unit/BusinessTable.spec.ts
@@ -0,0 +1,169 @@
+import { AmlRoles, AmlTypes } from '@/enums'
+import { wrapperFactory } from '../vitest-wrapper-factory'
+import BusinessTable from '@/components/Amalgamation/BusinessTable.vue'
+import { CorpTypeCd } from '@bcrs-shared-components/corp-type-module'
+
+describe('Business Table', () => {
+  it('displays correctly when there are no amalgamating businesses', () => {
+    const wrapper = wrapperFactory(
+      BusinessTable,
+      null,
+      {
+        amalgamation: {
+          amalgamatingBusinesses: []
+        }
+      }
+    )
+
+    const table = wrapper.find('#business-table')
+    expect(table.exists()).toBe(true)
+
+    // verify table headers
+    const th = table.findAll('thead tr > th')
+    expect(th.length).toBe(6)
+    expect(th.at(0).text()).toBe('Business Name')
+    expect(th.at(1).text()).toBe('Business Type')
+    expect(th.at(2).text()).toBe('Mailing Address')
+    expect(th.at(3).text()).toBe('Role')
+    expect(th.at(4).text()).toBe('Status')
+    expect(th.at(5).text()).toBe('Action')
+
+    // verify table data
+    const td = table.findAll('tbody tr > td')
+    expect(td.length).toBe(1)
+    expect(td.at(0).attributes('colspan')).toBe('6')
+    expect(td.at(0).text()).toBe('No businesses added')
+
+    wrapper.destroy()
+  })
+
+  const amalgamatingBusinesses = [
+    {
+      label: 'BC Limited holding business',
+      type: AmlTypes.LEAR,
+      identifier: 'BC1111111',
+      name: 'My BC Limited Company',
+      email: 'bc1111111@example.com',
+      address: {
+        streetAddress: '123 Main St',
+        addressCity: 'Victoria',
+        addressCountry: 'CA',
+        postalCode: 'V8V 8V8'
+      },
+      legalType: CorpTypeCd.BC_COMPANY,
+      expectedBusinessType: 'BC Limited Company',
+      role: AmlRoles.HOLDING
+    },
+    {
+      label: 'Benefit Company amalgamating business with no address',
+      type: AmlTypes.LEAR,
+      identifier: 'BC2222222',
+      name: 'My Benefit Company',
+      email: 'bc2222222@example.com',
+      address: undefined,
+      legalType: CorpTypeCd.BENEFIT_COMPANY,
+      expectedBusinessType: 'BC Benefit Company',
+      role: AmlRoles.AMALGAMATING
+    },
+    {
+      label: 'foreign business in Federal jurisdiction',
+      type: AmlTypes.FOREIGN,
+      corpNumber: 'CA-3333333',
+      legalName: 'My Federal Business',
+      expectedBusinessType: 'Foreign',
+      foreignJurisdiction: {
+        country: 'CA',
+        region: 'FEDERAL'
+      },
+      expectedJurisdiction: 'Federal, Canada',
+      role: AmlRoles.AMALGAMATING
+    },
+    {
+      label: 'foreign business in USA jurisdiction',
+      type: AmlTypes.FOREIGN,
+      corpNumber: 'US-4444444',
+      legalName: 'My USA Business',
+      expectedBusinessType: 'Foreign',
+      foreignJurisdiction: {
+        country: 'US'
+      },
+      expectedJurisdiction: 'United States of America',
+      role: AmlRoles.AMALGAMATING
+    }
+  ]
+
+  for (const business of amalgamatingBusinesses) {
+    it(`displays correctly when there is a ${business.label}`, () => {
+      const wrapper = wrapperFactory(
+        BusinessTable,
+        null,
+        {
+          amalgamation: {
+            amalgamatingBusinesses: [ business ]
+          }
+        }
+      )
+
+      const table = wrapper.find('#business-table')
+      expect(table.exists()).toBe(true)
+
+      // verify table headers
+      const th = table.findAll('thead tr > th')
+      expect(th.length).toBe(6)
+      expect(th.at(0).text()).toBe('Business Name')
+      expect(th.at(1).text()).toBe('Business Type')
+      expect(th.at(2).text()).toBe('Mailing Address')
+      expect(th.at(3).text()).toBe('Role')
+      expect(th.at(4).text()).toBe('Status')
+      expect(th.at(5).text()).toBe('Action')
+
+      // verify table data
+      const td = table.findAll('tbody tr > td')
+      expect(td.length).toBe(6)
+      expect(td.at(0).classes('business-name')).toBe(true)
+      expect(td.at(1).classes('business-type')).toBe(true)
+      expect(td.at(2).classes('business-address')).toBe(true)
+      expect(td.at(3).classes('business-role')).toBe(true)
+      expect(td.at(4).classes('business-status')).toBe(true)
+      expect(td.at(5).classes('business-actions')).toBe(true)
+
+      if ((business.type === AmlTypes.LEAR)) {
+        expect(td.at(0).text()).toContain(business.name)
+        expect(td.at(0).text()).toContain(business.email)
+
+        expect(td.at(1).text()).toContain(business.expectedBusinessType)
+
+        if (business.address) {
+          expect(td.at(2).text()).toContain(business.address.streetAddress)
+          expect(td.at(2).text()).toContain(business.address.addressCity)
+          expect(td.at(2).text()).toContain('Canada')
+          expect(td.at(2).text()).toContain(business.address.postalCode)
+        } else {
+          expect(td.at(2).text()).toBe('Affiliate to view')
+        }
+
+        if (business.role === AmlRoles.AMALGAMATING) {
+          expect(td.at(3).text()).toBe('Amalgamating Business')
+        }
+        if (business.role === AmlRoles.HOLDING) {
+          expect(td.at(3).text()).toBe('Holding Company')
+        }
+
+        expect(td.at(4).exists()).toBe(true) // see separate BusinessTableStatus tests
+
+        expect(td.at(5).find('.v-btn').exists()).toBe(true)
+      }
+
+      if ((business.type === AmlTypes.FOREIGN)) {
+        expect(td.at(0).text()).toBe(business.legalName)
+        expect(td.at(1).text()).toBe(business.expectedBusinessType)
+        expect(td.at(2).text()).toBe(business.expectedJurisdiction)
+        expect(td.at(3).text()).toBe('Amalgamating Business')
+        expect(td.at(4).exists()).toBe(true) // see separate BusinessTableStatus tests
+        expect(td.at(5).find('.v-btn').exists()).toBe(true)
+      }
+
+      wrapper.destroy()
+    })
+  }
+})
