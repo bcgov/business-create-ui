b442817f3dfcbe82be0b26b70c2c3f830998b1ea
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2024-02-06 13:06:19 -0800

    19041 Created constants (enum) for resource phrases (#647)
    * - app version = 5.8.9
    - created constants (enum) for common resource phrases
    - updated misc resource files to use constants
    
    * - changed OFFENSE -> OFFENCE
    
    ---------
    
    Co-authored-by: Severin Beauvais <severin.beauvais@gov.bc.ca>

--- src/resources/Incorporation/BEN.ts
+++ src/resources/Incorporation/BEN.ts
@@ -2,6 +2,7 @@ import { IncorporationResourceIF } from '@/interfaces'
 import { FilingCodes, RuleIds } from '@/enums'
 import { CorpTypeCd, GetCorpFullDescription } from '@bcrs-shared-components/corp-type-module'
 import { IncorporationStepsCorp } from './steps'
+import { ResourcePhrases } from '../ResourcePhrases'
 
 export const IncorporationResourceBen: IncorporationResourceIF = {
   entityType: CorpTypeCd.BENEFIT_COMPANY,
@@ -114,17 +115,11 @@ export const IncorporationResourceBen: IncorporationResourceIF = {
         `The Company Articles and the Incorporation Agreement both contain a signature
           line for each person identified as an incorporator in the Incorporation Application
           with the name of that person set out legibly under the signature line,`,
-
-        'An original signature has been placed on each of those signature lines,',
-
-        `I have no reason to believe that the signature placed on a signature line is not the
-          signature of the person whose name is set out under that signature line, and`,
-
-        'I have relevant knowledge of the company and that I am authorized to make this filing.'
+        ResourcePhrases.ORIGINAL_SIGNATURE,
+        ResourcePhrases.BELIEVE_SIGNATURE,
+        ResourcePhrases.RELEVANT_KNOWLEDGE_OF_COMPANY
       ],
-      certifyClause: `Note: It is an offence to make a false or misleading statement in respect
-        of a material fact in a record submitted to the Corporate Registry for filing.
-        See section 427 of the Business Corporations Act.`,
+      certifyClause: ResourcePhrases.OFFENCE_SECTION_427,
       entityDisplay: null
     }
   }
