02155f944b06b51507630494bbdff1fd42d21b9a
ketaki-deodhar <ketaki.deodhar@gov.bc.ca>
2024-05-14 10:24:41 -0700

    20989-add manual business 4

--- src/views/ContinuationIn/ContinuationInBusinessHome.vue
+++ src/views/ContinuationIn/ContinuationInBusinessHome.vue
@@ -67,7 +67,6 @@ import { RouteNames } from '@/enums'
 import ContinuationAuthorization from '@/components/ContinuationIn/ContinuationAuthorization.vue'
 import ExtraproRegistration from '@/components/ContinuationIn/ExtraproRegistration.vue'
 import ManualBusinessInfo from '@/components/ContinuationIn/ManualBusinessInfo.vue'
-import { ExistingBusinessInfoIF } from '@/interfaces'
 
 @Component({
   components: {
@@ -80,8 +79,6 @@ export default class ContinuationInBusinessHome extends Mixins(CommonMixin, Name
   @Getter(useStore) getShowErrors!: boolean
 
   @Action(useStore) setContinuationInBusinessHomeValid!: (x: boolean) => void
-  @Action(useStore) setExistingBusinessInfo!: (x: ExistingBusinessInfoIF) => void
-
   @Action(useStore) setIgnoreChanges!: (x: boolean) => void
 
   // Local properties
