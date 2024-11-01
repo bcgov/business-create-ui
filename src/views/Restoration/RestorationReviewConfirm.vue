b022942571b397ec56caae41609dc5a1bd1e1b36
AimeeGao <Aimee.Gao@gov.bc.ca>
2024-11-01 15:37:00 -0700

    Allow editing of certify legal name for all users in Restoration

--- src/views/Restoration/RestorationReviewConfirm.vue
+++ src/views/Restoration/RestorationReviewConfirm.vue
@@ -101,7 +101,7 @@
         <Certify
           class="py-8 px-6"
           :class="{ 'invalid-section': isCertifyInvalid }"
-          :disableEdit="!isRoleStaff"
+          :disableEdit="false"
           :invalidSection="isCertifyInvalid"
           :isStaff="isRoleStaff"
         />
