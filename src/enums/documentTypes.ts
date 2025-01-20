e47352b3332c431b9e063523b73032b808a3dfb8
flutistar <brandonagalli@gmail.com>
2025-01-20 08:18:32 -0800

    created document type enum

--- src/enums/documentTypes.ts
+++ src/enums/documentTypes.ts
@@ -1,3 +1,19 @@
-export enum DocumentClasses {
+export enum DocumentClassEnum {
   CORP = 'CORP'
+}
+
+export enum DocumetTypeEnum {
+  CNTO = 'CNTO',
+  DIRECTOR_AFFIDAVIT = 'DIRECTOR_AFFIDAVIT'
+}
+
+export const DOCUMENT_TYPES = {
+  contInAuthorization: {
+    class: DocumentClassEnum.CORP,
+    type: DocumetTypeEnum.CNTO
+  },
+  affidavitDocument: {
+    class: DocumentClassEnum.CORP,
+    type: DocumetTypeEnum.DIRECTOR_AFFIDAVIT
+  }
 }
